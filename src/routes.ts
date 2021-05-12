f19c52fed758da1186f221272d4d64ae70784fed
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-05-12 11:46:00 -0300

    Clientes - Update

--- src/routes.ts
+++ src/routes.ts
@@ -6,8 +6,9 @@ const routes = Router();
 const clientesController = new ClientesController();
 
 routes.post("/clientes", clientesController.create);
+routes.put("/clientes/:idCliente", clientesController.update);
 routes.get("/clientes", clientesController.findAll);
-routes.get("/clientes/:email", clientesController.findByEmail);
+routes.get("/clientes/email", clientesController.findByEmail);
 routes.get("/clientes/:idCliente", clientesController.findByID);
 
 export { routes };
